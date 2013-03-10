96dcefbc7ea456a8721eaf18181587b0fdb05243
JD <artwolf@ns390450.ovh.net>
2013-03-10 05:21:16 +0100

    Mapquest API

---
+++ mapquest/mapquest.php
@@ -0,0 +1,11 @@
+<?php
+$string = 'http://www.mapquestapi.com/directions/v1/route?key=Fmjtd%7Cluub2l61n9%2Cb0%3Do5-96y2gw&callback=renderAdvancedNarrative&ambiguities=ignore&avoidTimedConditions=false&doReverseGeocode=true&outFormat=json&routeType=fastest&timeType=1&enhancedNarrative=false&shapeFormat=raw&generalize=0&locale=en_US&unit=m&from=' . str_replace(' ', '%20',$_GET["from"]) . '&to=' . str_replace(' ', '%20',$_GET["to"]) . '&drivingStyle=2&highwayEfficiency=21.0';
+
+$file = file_get_contents($string, true);
+
+$jsonfile = substr($file, strpos($file, "route") -2, -2);
+
+$object = json_decode($jsonfile);
+
+echo $object->route->time;
+?>
