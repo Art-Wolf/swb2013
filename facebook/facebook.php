89fd3da87f92b455ff6517b069adc6f2bd0be5d4
JD <artwolf@ns390450.ovh.net>
2013-03-10 05:20:40 +0100

    Facebook API

---
+++ facebook/facebook.php
@@ -0,0 +1,51 @@
+<?php
+require 'facebook-php-sdk/src/facebook.php';
+
+$facebook = new Facebook(array(
+  'appId'  => '131919856988146',
+  'secret' => '7707b9a0130f0833ac04ee499bd666a3',
+));
+
+    $access_token = $facebook->getAccessToken();
+    echo($access_token);
+    $me = null; 
+
+   if ($facebook->getUser() === 0 ) {
+   	header("Location:{$facebook->getLoginUrl(array('req_perms' => 'email,offline_access'))}"); 
+	}
+    try 
+    { 
+        $uid = $facebook->getUser(); 
+        echo "UID: " . $uid;
+        $me = $facebook->api('/'.$uid); 
+        echo "Welcome User: " . $me['name'] . "<br />"; 
+        //access permission
+        $permissions_needed = array('publish_stream', 'read_stream', 'offline_access', 'manage_pages');
+       // foreach($permissions_needed as $perm) 
+       // {  
+       //     if( !isset($permissions_list['data'][0][$perm]) || $permissions_list['data'][0][$perm] != 1 )
+       //     {    
+       //     $login_url_params = array(
+       //         'scope' => 'publish_stream,read_stream,offline_access,manage_pages',         
+       //         'fbconnect' =>  1,         
+       //         'display'   =>  "page",         
+       //         'next' => 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']      
+       //         );    
+       //     $login_url = $facebook->getLoginUrl($login_url_params);   
+       //     header("Location: {$login_url}");
+       //     exit(); 
+       //     }
+       // }
+        //Access permission
+
+        $post_id = $facebook->api("/$uid/feed", "post", array("message"=>"Hello World!")); 
+        if(isset($post_id)) 
+        {
+            echo "A new post to your wall has been posted with id: $post_id"; 
+        }
+    } 
+    catch (FacebookApiException $e) 
+    { 
+        echo($e); 
+    } 
+?>
